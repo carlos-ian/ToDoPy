ad093480f88d0ce183a69ca64477567d0dccfaac
Ian Carlos Lima Tavares <xxxiancarlosxxx@gmail.com>
2026-02-18 10:40:01 -0300

    Adicionado atualizações com a Rotas de Update e Delete de tarefas

--- ToDoPy/task_routes.py
+++ ToDoPy/task_routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi import Depends, status
+from fastapi import Depends, status, HTTPException
 from sqlalchemy.orm import Session
 import database, schemas, models
 from security import get_current_user
@@ -26,3 +26,26 @@ def create_task(task: schemas.TaskCreate, db: Session = Depends(database.get_db)
 @task_router.get("/get", response_model=list[schemas.TaskResponse])
 def read_task(db: Session = Depends(database.get_db), current_user: models.User = Depends(get_current_user)):
     return db.query(models.Task).filter(models.Task.owner_id == current_user.id).all()
+
+@task_router.patch("/{task_id}/status")
+def update_task(task_id: int, new_status: str, db: Session = Depends(database.get_db), current_user: models.User = Depends(get_current_user)):
+    task = db.query(models.Task).filter(models.Task.owner_id == current_user.id, models.Task.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+    
+    task.status = new_status
+    db.commit()
+    db.refresh(task)
+
+    return {"message": "Status Atualizado com Sucesso", "status": task.status}
+
+@task_router.delete("/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(database.get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+    
+    db.delete(task)
+    db.commit()
+
+    return {"message": "Tarefa deletada com sucesso"}
